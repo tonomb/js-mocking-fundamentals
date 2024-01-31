61c6c8ef81f505fa9d26fc43be24325f86f1e377
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-01-31 14:41:45 -0600

    completed exercises

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -14,20 +14,29 @@
  * Execute: Use `npx jest --watch src/__tests__/mock-fn.js` to watch the test
  */
 
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+test("returns winner", () => {
+  const originalGetWinner = utils.getWinner;
+  utils.getWinner = jest.fn((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+  expect(winner).toBe("Kent C. Dodds");
+
+  // console.log(utils.getWinner.mock.calls);
+
   // Your code:
+  expect(utils.getWinner.mock.calls).toEqual([
+    ["Kent C. Dodds", "Ken Wheeler"],
+    ["Kent C. Dodds", "Ken Wheeler"],
+  ]);
+  expect(utils.getWinner).toHaveBeenCalledTimes(2);
+  expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler");
 
   // cleanup
-  utils.getWinner = originalGetWinner
-})
+  utils.getWinner = originalGetWinner;
+});
 
 /**
  * Hints:
--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -6,24 +6,34 @@
  * Execute: Use `npx jest --watch src/no-framework/mock-fn.js` to watch the test
  */
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
 // Your Code:
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = fn((p1, p2) => p1)
+function fn(impl) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+  mockFn.mock = { calls: [] };
+  return mockFn;
+}
+
+const originalGetWinner = utils.getWinner;
+utils.getWinner = fn((p1, p2) => p1);
+
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+  ["Kent C. Dodds", "Ken Wheeler"],
+  ["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner = originalGetWinner;
 
 /**
  * Checkout master branch to see the answer.
