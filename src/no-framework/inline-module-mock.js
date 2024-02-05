c29216a8f2581ab6a4a58ea87dbfa316c92703b0
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-02-05 15:10:41 -0600

    complets Mock a JS module in a test

--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -4,27 +4,38 @@
  * Execute: Use `npx jest --watch src/no-framework/inline-module-mock.js` to watch the test
  */
 
+const utilsPath = require.resolve("../utils");
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    getWinner: fn((p1, p2) => p1),
+  },
+};
+
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
+
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  return mockFn;
 }
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
-
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+  ["Kent C. Dodds", "Ken Wheeler"],
+  ["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
+delete require.cache[utilsPath];
 
 /**
  * Hints:
